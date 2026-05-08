d7153fd8f2207c17538c261d72cdac9ebe68022c
Sam <41166645+koketsosamuel@users.noreply.github.com>
2026-05-08 23:15:59 +0200

    Add Slimkay launch configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://slimkay.co.za',
   output: 'static',
+  trailingSlash: 'always',
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/404/'),
+    }),
+  ],
 });
